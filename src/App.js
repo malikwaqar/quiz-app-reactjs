501ebcd6ed0abf651972269555dda07bb83a7207
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-12-23 05:14:06 +0500

    Quiz App Complete with score

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import questions from "./components/data";
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const questionHandler = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score">
+          Your Score: {score}/{questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="container">
+            <p>
+              Question # {currentQuestion + 1}/{questions.length}
+            </p>
+            <h3>{questions[currentQuestion].question}</h3>
+            <div className="answers">
+              {questions[currentQuestion].options.map((option) => (
+                <button onClick={() => questionHandler(option.isCorrect)}>
+                  {option.answer}
+                </button>
+              ))}
+            </div>
+            {/* <button>Score: {0}</button> */}
+            {console.log(questions)}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,130 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: [
+      { answer: "New York", isCorrect: false },
+      { answer: "London", isCorrect: false },
+      { answer: "Paris", isCorrect: true },
+      { answer: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of Pakistan?",
+    options: [
+      { answer: "Karachi", isCorrect: false },
+      { answer: "Lahore", isCorrect: false },
+      { answer: "Quetta", isCorrect: false },
+      { answer: "Islamabad", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is the capital of India?",
+    options: [
+      { answer: "Mumbai", isCorrect: false },
+      { answer: "Kolkatta", isCorrect: false },
+      { answer: "New Delhi", isCorrect: true },
+      { answer: "Banglore", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of England?",
+    options: [
+      { answer: "Manchester", isCorrect: false },
+      { answer: "London", isCorrect: true },
+      { answer: "Bristol", isCorrect: false },
+      { answer: "Birmingham", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of Australia?",
+    options: [
+      { answer: "Sydney", isCorrect: false },
+      { answer: "Perth", isCorrect: false },
+      { answer: "Melbourne", isCorrect: false },
+      { answer: "Canberra", isCorrect: true },
+    ],
+  },
+  {
+    question: "Who is CEO of Tesla?",
+    options: [
+      { answer: "Jeff Bezos", isCorrect: false },
+      { answer: "Elon Musk", isCorrect: true },
+      { answer: "Bill Gates", isCorrect: false },
+      { answer: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    question: "The iPhone was created by which company?",
+    options: [
+      { answer: "Apple", isCorrect: true },
+      { answer: "Intel", isCorrect: false },
+      { answer: "Amazon", isCorrect: false },
+      { answer: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    question: "How many Harry Potter books are there?",
+    options: [
+      { answer: "1", isCorrect: false },
+      { answer: "4", isCorrect: false },
+      { answer: "6", isCorrect: false },
+      { answer: "7", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is the capital of Newzealand?",
+    options: [
+      { answer: "Wellington", isCorrect: true },
+      { answer: "Aukland", isCorrect: false },
+      { answer: "Hamilton", isCorrect: false },
+      { answer: "Christchurch", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of Beljium?",
+    options: [
+      { answer: "Ghent", isCorrect: false },
+      { answer: "Brussels", isCorrect: true },
+      { answer: "Antwerp", isCorrect: false },
+      { answer: "Bruges", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of Canada?",
+    options: [
+      { answer: "Vancouver", isCorrect: false },
+      { answer: "Montreal", isCorrect: false },
+      { answer: "Toronto", isCorrect: false },
+      { answer: "Ottawa", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is the capital of China?",
+    options: [
+      { answer: "Shanghai", isCorrect: false },
+      { answer: "Beijing", isCorrect: true },
+      { answer: "Shenzen", isCorrect: false },
+      { answer: "Wuhan", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the capital of Germany?",
+    options: [
+      { answer: "Frankfurt", isCorrect: false },
+      { answer: "Hamburg", isCorrect: false },
+      { answer: "Munich", isCorrect: false },
+      { answer: "Berlin", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is the capital of Srilanka?",
+    options: [
+      { answer: "Kandey", isCorrect: false },
+      { answer: "Colombo", isCorrect: true },
+      { answer: "Galle", isCorrect: false },
+      { answer: "Jaffna", isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
